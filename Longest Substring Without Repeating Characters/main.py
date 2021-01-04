1f223099a3d5de9134643b0cf44e5abba1c439a0
PromasterGuru <=>
2021-01-04 08:18:52 +0300

    Longest Substring Without Repeating Characters

---
+++ Longest Substring Without Repeating Characters/main.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        maxs = 0
+        temp = ""
+        l = 0
+        for i in range(len(s)):
+            if s[i] in temp:
+                while temp[l] != s[i]:
+                    l+=1
+                temp = temp[l+1:]
+            temp += s[i]
+            l = 0
+            if len(temp) > maxs:
+                maxs = len(temp)
+        return maxs
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = ["abcabcbb","bbbbb","pwwkew",""," ","dvdf","tmmzuxt"]
+    for s in tests:
+        print(sol.lengthOfLongestSubstring(s))
